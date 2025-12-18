06dcebe20ad00f9042d46036557141ad029f70e4
Sasanga Chathumal <chathumal.sasanga02@gmail.com>
2025-12-18 18:45:45 +0530

    re enable the open command and fixed error messages.

--- index.js
+++ index.js
@@ -11,11 +11,7 @@
 
 const { program } = require('commander');
 const { run: createProject } = require('./src/cli');
-const { listCommand } = require('./src/commands/list');
-const { recentCommand } = require('./src/commands/recent');
 const { openCommand } = require('./src/commands/open');
-const { bookmarkCommand } = require('./src/commands/bookmark');
-const { statsCommand } = require('./src/commands/stats');
 const logger = require('./src/utils/logger');
 
 // Package info
@@ -33,46 +29,12 @@ program
     createProject();
   });
 
-// List all projects
-program
-  .command('list')
-  .alias('ls')
-  .description('List all tracked projects')
-  .action(() => {
-    listCommand();
-  });
-
-// Show recent projects
-program
-  .command('recent')
-  .description('Show recently accessed projects')
-  .argument('[limit]', 'number of projects to show', '10')
-  .action((limit) => {
-    recentCommand(parseInt(limit));
-  });
-
 // Open project
 program
-  .command('open <project-name>')
+  .command('open [project-name]')
   .description('Open a project in IDE')
   .action((projectName) => {
     openCommand(projectName);
-  });
-
-// Bookmark commands
-program
-  .command('bookmark [action] [project-name]')
-  .description('Manage bookmarked projects')
-  .action((action, projectName) => {
-    bookmarkCommand(action, projectName);
-  });
-
-// Show statistics
-program
-  .command('stats')
-  .description('Show project statistics')
-  .action(() => {
-    statsCommand();
   });
 
 // Handle errors
